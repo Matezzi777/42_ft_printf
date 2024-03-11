5a804c4f4626a119ce2e7064c8e0e303318dedb8
Matezzi777 <matezzi777@gmail.com>
2024-03-12 00:10:00 +0100

    ft_printf.h READY

--- ft_printf.h
+++ ft_printf.h
@@ -2,14 +2,13 @@
 # define FT_PRINTF_H
 
 # include <stdarg.h> //fonction variadique
-# include <stdlib.h> //malloc(), free()
 # include <unistd.h> //write()
 
+int	ft_printf(const char *format, ...);
 int	ft_print_character(int c);
 int	ft_print_hexadecimal(unsigned int number, char flag);
 int	ft_print_number(int number);
 int	ft_print_pointer(unsigned long long pointer);
 int	ft_print_string(char *string);
-int	ft_print_unsigned(unsigned int number);
 
 #endif
