9a4e23fa33a19512df34351bee8942670194ee73
Matezzi777 <matezzi777@gmail.com>
2024-03-12 00:09:29 +0100

    ft_print_pointer.c READY

--- ft_print_pointer.c
+++ ft_print_pointer.c
@@ -16,5 +16,15 @@ int	ft_print_pointer(unsigned long long pointer);
 
 int	ft_print_pointer(unsigned long long pointer)
 {
-	
+	int	written_chars;
+	int	adress_hexa;
+
+	if (ft_print_string("0x" == -1))
+		return (-1);
+	written_chars = 2;
+	adress_hexa = ft_print_hexadecimal((unsigned long)pointer, 'x');
+	if (adress_hexa == -1)
+		return (-1);
+	written_chars += adress_hexa;
+	return (written_chars);
 }
